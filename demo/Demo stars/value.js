454c0e35cc0105fef33b77ca897897575948b4ff
DaniloMolina <35104046+apachaii@users.noreply.github.com>
2020-07-01 02:24:47 -0400

    Add files via upload

---
+++ demo/Demo stars/value.js
@@ -0,0 +1,16 @@
+
+let product2=document.getElementById('img1').getAttribute('value');
+
+class SelectOption extends HTMLElement{
+
+    constructor(){
+        super()
+    }
+
+    connectedCallback() {
+        this.innerHTML =`<h1>${product2}</h1>`
+       }
+}
+
+
+window.customElements.define('select-one',SelectOption)
